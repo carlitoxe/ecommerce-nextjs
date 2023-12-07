281f1098ccc5d8dfafdb831d6e840ede3ba7a978
carlitoxe <cerc-12@hotmail.com>
2023-12-07 18:52:39 -0400

    Add SkeletonLoading

--- src/components/SkeletonLoading/index.tsx
+++ src/components/SkeletonLoading/index.tsx
@@ -6,7 +6,7 @@ export default function SkeletonLoading({ qty }: { qty: number}) {
     .map((_, i) => (
       <div key={i} role="status" className="w-56 h-[308px] border rounded-lg border-gray-200 shadow animate-pulse dark:border-gray-700 text-center">
         <figure className="flex items-center justify-center w-full h-[224px] mb-3 bg-gray-300 rounded-t-lg dark:bg-gray-700 ">
-            <PhotoIcon className="w-14 h-14 text-gray-200 dark:text-gray-600 rounded-t-lg" />
+            <PhotoIcon className="w-14 h-14 text-gray-200 dark:text-gray-600" />
         </figure>
         <div className="flex flex-col items-center justify-center">
             <span className="w-52 h-4 bg-gray-200 rounded-full dark:bg-gray-700 mb-3"></span>
